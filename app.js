3e457fa82d69a1c50f9d9ca5f30e0c15b052b916
NathanWeber <nathanpatrike@gmail.com>
2021-04-19 03:33:24 -0300

    Ajuste handlebars e validação formulário cadastro de usuários

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
-const express = require('express');
+const express = require('express')
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
-const path = require('path');
+const path = require('path')
 const app = express()
 const mongoose = require('mongoose');
+const session = require('express-session')
+const flash = require('connect-flash')
 require("./models/usuario")
 const Usuario = mongoose.model("usuario")
 
+function teste(){
+    console.log("vai se fuder")
+}
 //Configurações
+//Sessão
+app.use(session({
+    secret:"superseguro",
+    resave: true,
+    saveUninitialized: true
+}))
+app.use(flash())
+//Middleware
+app.use((req, res, next) =>{
+    res.locals.success_msg = req.flash("success_msg")
+    res.locals.error_msg = req.flash("error_msg")
+    next()
+})
 //Body Parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -23,38 +41,56 @@ mongoose.connect("mongodb://localhost/jogocovid19").then(()=>{
     });
 
 //Public
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")))
 
 //Rotas
-
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/login.html'));
+    res.render("layouts/login")
 } )
 app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname + '/login.html'));
+    //res.sendFile(path.join(__dirname + '/login.html'));
+    res.render("layouts/login")
 } )
 app.get('/cadastro', (req, res) => {
-    res.sendFile(path.join(__dirname + '/cadastro.html'));
+    //res.sendFile(path.join(__dirname + '/views/layouts/cadastro.handlebars'));
+    res.render("layouts/cadastro")
 } )
 app.get('/home', (req, res) => {
-    res.sendFile(path.join(__dirname + '/home.html'));
+    res.render("layouts/home")
 } )
 app.get("/nivel1", function (req, res) {
-    res.sendFile(path.join(__dirname + '/nivel1.html'));
+    res.render("layouts/nivel1")
 });
 app.get("/nivel2", function (req, res) {
-    res.sendFile(path.join(__dirname + '/nivel2.html'));
+    res.render("layouts/nivel2")
 });
 app.get("/nivel3", function (req, res) {
-    res.sendFile(path.join(__dirname + '/nivel3.html'));
+    res.render("layouts/nivel3")
 });
 app.get("/ranking", function (req, res) {
-    res.sendFile(path.join(__dirname + '/nivel1.html'));
+    res.render("layouts/ranking")
 });
 app.get("/criadores", function (req, res) {
-    res.sendFile(path.join(__dirname + '/criadores.html'));
+    res.render("layouts/criadores")
 });
+
 app.post('/novocadastro', (req, res) => {
+    var erros = []
+
+    if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+        erros.push({texto: "Nome inválido"})
+    }
+    if(!req.body.sobrenome || typeof req.body.sobrenome == undefined || req.body.sobrenome == null){
+        erros.push({texto: "Sobre nome inválido"})
+    }
+    if(req.body.senha.length < 6){
+        erros.push({texto: "Senha deve ter no mínimo 6 caracteres!"})
+    }
+
+    if(erros.length > 0){
+        res.render("layouts/cadastro", {erros: erros})
+    }   
+
    const novoUsuario = {
        nome: req.body.nome,
        sobrenome: req.body.sobrenome,
@@ -63,11 +99,14 @@ app.post('/novocadastro', (req, res) => {
    }
 
    new Usuario(novoUsuario).save().then(()=>{
-    console.log('Usuário cadastrado com sucesso!');
+    console.log('Usuário cadastrado com sucesso!')
+    req.flash("success_msg", "Usuário cadastrado com sucesso!")
+    res.redirect("/login")
     }).catch(()=>{
-    console.log('Erro ao cadastrar Usuário!');
+    console.log('Erro ao cadastrar Usuário!')
+    req.flash("error_msg", "Erro ao cadastrar usuário!")
     });
-} )
+});
 
 //Outros
 const PORT = 8081
